71190b3ceca31459c1810c6d68d9258ec3b857d9
Rzv888 <udinamin445@gmail.com>
2023-03-05 19:24:23 +0700

    tambahan fitur GUI

--- asik/Form1.cs
+++ asik/Form1.cs
@@ -6,5 +6,16 @@ namespace asik
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String tombol = textBox1.Text;
+            label1.Text = "Halo" + tombol;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
